1bac837c71a6dde42d0db8950148bbe57aed88ed
Bam-samada <146070799+Bam-samada@users.noreply.github.com>
2024-05-30 17:29:40 +0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -22,7 +22,7 @@ router.use(function timeLog(req, res, next) {
 });
 
 var conn = mysql.createConnection({
-  host: process.env.DB_HOST,
+  host: "mysql",
   user: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
