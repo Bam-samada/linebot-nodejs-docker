89dc780756b99ad3a2bde7edd678072cc850a03c
Bam-samada <146070799+Bam-samada@users.noreply.github.com>
2024-05-30 17:40:15 +0700

    Create test.js

---
+++ routes/test.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+const express = require("express"); // เรียกใช้งาน express  mudule
+const router = express.Router(); // กำหนด router instance ให้กับ express.Router class
+const mysql = require("mysql2");
+const _ = require("lodash");
+
+router.use(function timeLog(req, res, next) {
+  console.log("Time: ", Date.now());
+  next();
+});
+
+var conn = mysql.createConnection({
+  host: "mysql",
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+conn.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + conn.threadId);
+});
+
+module.exports = router;
